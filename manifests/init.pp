2efc5601e768487fa0ed0818ff797f6f10d6eae6
Alberto Garcia <alberto.garcia@rentalia.com>
2014-07-07 14:56:36 +0200

    perlbrew from packages

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,57 +1,63 @@
 class perlbrew (
-
+1
   $perl,
   $perl_use,
   $perl_modules,
-  $debian_packages
+  $debian_packages,
+  $user,
 
   ) {
 
   package { $debian_packages:
     ensure  => latest,
-    before  => Exec['install_perlbrew'],
+    before  => Exec['set_source'],
   }
 
-  exec { 'install_perlbrew':
-    command => '/usr/bin/curl -L http://install.perlbrew.pl | /bin/bash',
-    creates => '/root/perl5/perlbrew/bin/perlbrew',
-    timeout => '0',
-  }
+  package { 'perlbrew': ensure  => latest }
 
-  exec { 'set source':
-    command => '/bin/echo \'source ~/perl5/perlbrew/etc/bashrc\' >> /root/.bashrc; /root/perl5/perlbrew/bin/perlbrew init',
-    unless  => '/bin/grep \'source ~/perl5/perlbrew/etc/bashrc\' /root/.bashrc',
-    require => Exec['install_perlbrew'],
-    before  => Exec['install_perl_version'],
+  exec { 'set_source':
+    command   => "/bin/echo \'source ~/perl5/perlbrew/etc/bashrc\' >> /home/${user}/.bashrc; /usr/bin/perlbrew init",
+    unless    => "/bin/grep \'source ~/perl5/perlbrew/etc/bashrc\' /home/$user/.bashrc",
+    user      => $user,
+    provider  => 'shell',
+    require   => Package['perlbrew'],
+    before    => Exec['install_perl_version'],
   }
 
   define install_perl {
     exec { 'install_perl_version':
-      command => "/root/perl5/perlbrew/bin/perlbrew install ${name}",
-      creates => "/root/perl5/perlbrew/perls/perl-${name}/bin/perl",
-      require => Exec['install_perlbrew'],
-      before  => Exec['set_perl'],
-      timeout => '0',
+      command   => "/usr/bin/perlbrew install ${name}",
+      user      => $user,
+      creates   => "/home/$user/perl5/perlbrew/perls/perl-${name}/bin/perl",
+      provider  => 'shell',
+      before    => Exec['set_perl'],
+      timeout   => '0',
     }
   }
 
   install_perl { $perl:}
 
   exec { 'set_perl':
-    command => "/root/perl5/perlbrew/bin/perlbrew switch perl-${perl_use}",
-    unless  => "/usr/bin/test -f /root/perl5/perlbrew/perls/${perl_use}/bin/perl",
+    command   => "/usr/bin/perlbrew switch perl-${perl_use}",
+    provider  => 'shell',
+    user      => $user,
+    unless    => "/usr/bin/test -f /root/perl5/perlbrew/perls/${perl_use}/bin/perl",
   }
 
   exec { 'install_cpanm':
-    command => '/root/perl5/perlbrew/bin/perlbrew install-cpanm',
-    require => Exec['set_perl'],
-    unless  => '/usr/bin/test -f /root/perl5/perlbrew/bin/cpanm',
-    timeout => '0',
+    command   => '/usr/bin/perlbrew install-cpanm',
+    require   => Exec['set_perl'],
+    user      => $user,
+    provider  => 'shell',
+    unless    => "/usr/bin/test -f /home/${user}/perl5/perlbrew/bin/cpanm",
+    timeout   => '0',
   }
 
   exec { 'install_modules':
-    command => "/root/perl5/perlbrew/bin/perlbrew init;/root/perl5/perlbrew/bin/perlbrew switch perl-${perl_use}|/root/perl5/perlbrew/bin/cpanm ${perl_modules}",
-    require => Exec['install_cpanm'],
-    timeout => '0',
+    command   => "/usr/bin/perlbrew init;usr/bin/perlbrew switch perl-${perl_use}|/usr/bin/perlbrew/bin/cpanm ${perl_modules}",
+    user      => $user,
+    provider  => 'shell',
+    require   => Exec['install_cpanm'],
+    timeout   => '0',
   }
 }
