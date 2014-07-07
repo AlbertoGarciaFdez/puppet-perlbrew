b4c21f313eb4771283e397f555dc2624ac13c06f
Alberto Garcia <alberto.garcia@rentalia.com>
2014-07-07 15:32:24 +0200

    perlbrew

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,20 +13,24 @@ class perlbrew (
     before  => Exec['set_source'],
   }
 
-  package { 'perlbrew': ensure  => latest }
+  exec { 'install_perlbrew':
+    command   => 'sudo apt-get install perlbrew',
+    provider  => 'shell',
+    user      => $user,
+  }
 
   exec { 'set_source':
-    command   => "/bin/echo \'source ~/perl5/perlbrew/etc/bashrc\' >> /home/${user}/.bashrc; /usr/bin/perlbrew init",
+    command   => "/bin/echo \'source ~/perl5/perlbrew/etc/bashrc\' >> /home/${user}/.bashrc; perlbrew init",
     unless    => "/bin/grep \'source ~/perl5/perlbrew/etc/bashrc\' /home/$user/.bashrc",
     user      => $user,
     provider  => 'shell',
-    require   => Package['perlbrew'],
+    require   => Exec['install_perlbrew'],
     before    => Exec['install_perl_version'],
   }
 
   define install_perl {
     exec { 'install_perl_version':
-      command   => "/usr/bin/perlbrew install ${name}",
+      command   => "perlbrew install ${name}",
       user      => $user,
       creates   => "/home/$user/perl5/perlbrew/perls/perl-${name}/bin/perl",
       provider  => 'shell',
@@ -38,14 +42,14 @@ class perlbrew (
   install_perl { $perl:}
 
   exec { 'set_perl':
-    command   => "/usr/bin/perlbrew switch perl-${perl_use}",
+    command   => "perlbrew switch perl-${perl_use}",
     provider  => 'shell',
     user      => $user,
     unless    => "/usr/bin/test -f /root/perl5/perlbrew/perls/${perl_use}/bin/perl",
   }
 
   exec { 'install_cpanm':
-    command   => '/usr/bin/perlbrew install-cpanm',
+    command   => 'perlbrew install-cpanm',
     require   => Exec['set_perl'],
     user      => $user,
     provider  => 'shell',
@@ -54,7 +58,7 @@ class perlbrew (
   }
 
   exec { 'install_modules':
-    command   => "/usr/bin/perlbrew init;usr/bin/perlbrew switch perl-${perl_use}|/usr/bin/perlbrew/bin/cpanm ${perl_modules}",
+    command   => "cpanm ${perl_modules}",
     user      => $user,
     provider  => 'shell',
     require   => Exec['install_cpanm'],
