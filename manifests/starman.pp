2b70546fe8a19d69a02e79c4c348236d15017ef5
Alberto Garcia <alberto.garcia@rentalia.com>
2014-07-16 13:02:52 +0200

    starman

---
+++ manifests/starman.pp
@@ -0,0 +1,30 @@
+class starman (
+
+  $scripts,
+  $workers,
+  $user,
+  $gorup,
+  $app,
+  $name,
+
+) {
+
+  file {'/etc/init.d/starman':
+    mode    => 744,
+    content => template('private_files/starman.erb'),
+    notify  => Service['starman']
+  }
+
+  exec {'add_daemon':
+    command => '/usr/sbin/update-rc.d starman defaults',
+    cwd => '/etc/init.d'
+    require => File ['/etc/init.d/starman']
+  }
+
+  service {'starman':
+    enable      => true,
+    ensure      => running,
+    hasrestart  => true,
+    require     => Exec['add_daemon'],
+  }
+}
